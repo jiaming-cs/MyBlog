0d85f49295b47d883719ba936bc3ef4ac40adc40
Jiaming Li <jiamingli9674@gmail.com>
2021-03-18 16:15:12 -0700

    add static to template

--- blog/src/main/java/com/jiaming/blog/web/IndexController.java
+++ blog/src/main/java/com/jiaming/blog/web/IndexController.java
@@ -4,18 +4,18 @@ import com.jiaming.blog.NotFoundException;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
 
 @Controller
 public class IndexController {
-    @GetMapping("/{name}/{id}")
-    public String index(@PathVariable String name, @PathVariable Integer id) throws NotFoundException {
+    @GetMapping("/")
+    public String index() throws NotFoundException {
         // int i = 1/0;
         // String blog = null;
 
         // if (blog == null) {
         //     throw new NotFoundException("Page Not Found");
         // }
-        return "Index"; // Name of html tamplate
+        return "index"; // Name of html tamplate
     }
 }
